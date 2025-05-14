510b9f152299d79805759d8cb9bee4ed3f9a306a
AdamKelly-STUDENT <s00252192@atu.ie>
2025-05-14 09:35:44 +0100

    Q1 (c)

---
+++ Q1/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Patient
+    {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DOB { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
