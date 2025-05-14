b65c566adb2eb5673b22ae453b2f4b1e6a5f54ae
AdamKelly-STUDENT <s00252192@atu.ie>
2025-05-14 09:42:47 +0100

    Q1 (e)

--- Q1/Classes.cs
+++ Q1/Classes.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Q1
-{
+{    class Appointment
+    {
+        public int AppointmentID { get; set; }
+        public DateTime AppointmentTime { get; set; }
+        public string AppointmentNotes { get; set; }
+
+        public int PatientID { get; set; }
+        public virtual Patient Patient { get; set; }
+    }
+
     class Patient
     {
         public int PatientID { get; set; }
@@ -13,5 +23,7 @@ namespace Q1
         public string Surname { get; set; }
         public DateTime DOB { get; set; }
         public string ContactNumber { get; set; }
+
+        public virtual List<Appointment> Appointments { get; set; }
     }
 }
